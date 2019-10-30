7be28ea20f55d5dbb66c3f6b363c42fff1dcfce1
JimiStephen <luxideyao@163.com>
2019-10-30 22:07:34 +0800

    spring-cloud zk 例子

---
+++ spcloud-zk/src/main/java/com/jimi/parent/springcloud/spcloudzk/SpcloudZkApplication.java
@@ -0,0 +1,49 @@
+package com.jimi.parent.springcloud.spcloudzk;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.zookeeper.serviceregistry.ServiceInstanceRegistration;
+import org.springframework.cloud.zookeeper.serviceregistry.ZookeeperRegistration;
+import org.springframework.cloud.zookeeper.serviceregistry.ZookeeperServiceRegistry;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class SpcloudZkApplication {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello world";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(SpcloudZkApplication.class, args);
+    }
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    public String serviceUrl() {
+        List<ServiceInstance> list = discoveryClient.getInstances("STORES");
+        if (list != null && list.size() > 0 ) {
+            return list.get(0).getUri().toString();
+        }
+        return null;
+    }
+
+    @Autowired
+    private ZookeeperServiceRegistry serviceRegistry;
+
+    public void registerThings() {
+        ZookeeperRegistration registration = ServiceInstanceRegistration.builder()
+                .defaultUriSpec()
+                .address("anyUrl")
+                .port(10)
+                .name("/a/b/c/d/anotherservice")
+                .build();
+        this.serviceRegistry.register(registration);
+    }
+}
---
+++ spcloud-zk/src/test/java/com/jimi/parent/springcloud/spcloudzk/SpcloudZkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jimi.parent.springcloud.spcloudzk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpcloudZkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
