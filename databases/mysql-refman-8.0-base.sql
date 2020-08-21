db03117b44a5002fd874de08efc025a7fc563806
xianyao.ye <xianyao.ye@ucarinc.com>
2020-08-21 16:43:29 +0800

    添加数据库操作文件

--- databases/mysql-refman-8.0-base.sql
+++ databases/mysql-refman-8.0-base.sql
@@ -75,7 +75,62 @@ show engines ;
 show status like 'Com_______';
 show status like 'Innodb_rows_%'
  
+ -- 慢日志查询 --log-slow-queries[=filename]
+ -- 命令查看当前MySQL在进行的线程，包括线程的状态、是否锁表等，
+ -- 可以实时地查看 SQL 的执行情况，同时对一些锁表操作进行优化。
+ show processlist;
  
+ -- explain 
+ -- type 表示表的连接类型，性能由好到差的连接类型为( system ---> const -----> eq_ref ------> ref
+-- -----> ref_or_null----> index_merge ---> index_subquery -----> range -----> index ------>
+-- all )
+
+show profile for query 6;
+-- 查看执行sql执行的耗时情况；
+show profiles;
+-- 通过 have_profiling 参数，能够看到当前MySQL是否支持profile：
+select @@have_profiling;
+-- 默认profiling是关闭的，可以通过set语句在Session级别开启profiling：
+-- set profiling=1// profilling 开关
+select @@profiling;
+-- MySQL5.6提供了对SQL的跟踪trace, 
+--  打开trace ， 设置格式为 JSON，并设置trace最大能够使用的内存大小，避免解析过程中因为默认内存过小而不能
+-- 够完整展示。
+SET optimizer_trace="enabled=on",end_markers_in_json=on;
+set optimizer_trace_max_mem_size=1000000;
+
+select * from actor where actor_id < 1000;
+select * from information_schema.optimizer_trace;
+
+-- 查看索引使用情况
+show status like 'Handler_read%';
+show global status like 'Handler_read%';
+-- Handler_read_first：索引中第一条被读的次数。如果较高，表示服务器正执行大量全索引扫描（这个值越低
+-- 越好）。
+-- Handler_read_key：如果索引正在工作，这个值代表一个行被索引值读的次数，如果值越低，表示索引得到的
+-- 性能改善不高，因为索引不经常使用（这个值越高越好）。
+-- Handler_read_next ：按照键顺序读下一行的请求数。如果你用范围约束或如果执行索引扫描来查询索引列，
+-- 该值增加。
+-- Handler_read_prev：按照键顺序读前一行的请求数。该读方法主要用于优化ORDER BY ... DESC。
+-- Handler_read_rnd ：根据固定位置读一行的请求数。如果你正执行大量查询并需要对结果进行排序该值较高。
+-- 你可能使用了大量需要MySQL扫描整个表的查询或你的连接没有正确使用键。这个值较高，意味着运行效率低，应
+-- 该建立索引来补救。
+-- Handler_read_rnd_next：在数据文件中读下一行的请求数。如果你正进行大量的表扫描，该值较高。通常说
+-- 明你的表索引不正确或写入的查询没有利用索引。
+
+-- 批量插入优化方法
+-- 主键顺序插入
+-- 关闭唯一校验  set UNIQUE_CHECKS=0;
+-- 手动提交事务  set autocommit=0; set autocommit=1;
+-- 忽略索引 
+explain select * from tb_seller ignore index(idx_seller_name) where name = '小米科
+技';
+-- 使用某个索引
+explain select * from tb_seller use index(idx_seller_name) where name = '小米科
+技';
+-- 强制使用索引 
+explain select * from tb_seller force index(idx_seller_name) where name = '小米科
+技';
 --  CREATE
 -- [DEFINER = { user | CURRENT_USER }]
 -- EVENT
